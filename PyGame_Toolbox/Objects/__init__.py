76cb687a6c93cf90ec2efcd8ae5d8469164875be
Nikodus <niklas@loidls.com>
2025-05-20 14:12:54 +0200

    Refactoring for better overview

---
+++ PyGame_Toolbox/Objects/__init__.py
@@ -0,0 +1,19 @@
+from .Object import Object
+from .Toolbar import Toolbar
+from .Invisible_Button import Invisible_Button
+from .Label import Label
+from .PictureBox import PictureBox
+from .Text_button import Text_button
+from .Textbox import TextBox
+
+
+
+__all__ = [
+    "Object",
+    "Toolbar",
+    "Invisible_Button",
+    "Label",
+    "PictureBox",
+    "Text_button",
+    "Textbox",
+]
